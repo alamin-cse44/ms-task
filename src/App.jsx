ae713ba36753ebca5062b828a42c303e535ddf9a
alamin-cse44 <alamincse44@gmail.com>
2024-01-07 01:44:28 +0600

    Chore: fetch api

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,36 @@ import Problem2 from "./components/Problem-2.jsx";
 import Index from "./components/Index.jsx";
 
 function App() {
+  const [allContacts, setAllContacts] = useState(null);
+  const [usContacts, setUsContacts] = useState(null);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  useEffect(() => {
+    fetch(`https://contact.mediusware.com/api/contacts/?page=${currentPage}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setAllContacts(data.results);
+        // console.log(data)
+      });
+  }, []);
+
+  useEffect(() => {
+    fetch(
+      `https://contact.mediusware.com/api/country-contacts/United%20States/?page=${currentPage}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setUsContacts(data.results);
+        // console.log(data.results);
+      });
+  }, []);
   return (
     <>
       <Routes>
         <Route path="/" element={<Index />} />
         <Route path="/" element={<Menu />}>
           <Route path="problem-1" element={<Problem1 />} />
-          <Route path="problem-2" element={<Problem2 />} />
+          <Route path="problem-2" element={<Problem2 contacts={allContacts} usa={usContacts} />} />
         </Route>
       </Routes>
     </>
