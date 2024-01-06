bdb29627862ab164f76d2445637fb476f65fa217
alamin-cse44 <alamincse44@gmail.com>
2024-01-07 01:52:24 +0600

    Add: modal

--- src/components/Problem-2.jsx
+++ src/components/Problem-2.jsx
@@ -1,21 +1,68 @@
-import React from 'react';
+import React, { useState } from "react";
+import Modal from "./problem-2/Modal";
 
-const Problem2 = () => {
+const Problem2 = ({contacts, usa}) => {
+  const [show, setShow] = useState("all");
+  const [showModal, setShowModal] = useState(false);
 
-    return (
+  const handleShow = (data) => {
+    setShowModal(true);
+    setShow(data);
+  };
 
-        <div className="container">
-            <div className="row justify-content-center mt-5">
-                <h4 className='text-center text-uppercase mb-5'>Problem-2</h4>
-                
-                <div className="d-flex justify-content-center gap-3">
-                <button className="btn btn-lg btn-outline-primary" type="button" >All Contacts</button>
-                <button className="btn btn-lg btn-outline-warning" type="button" >US Contacts</button>
-                </div>
-                
-            </div>
+  const handleClose = () => setShowModal(false);
+
+  return (
+    <div className="container test">
+      <div className="row justify-content-center mt-5">
+        <h4 className="text-center text-uppercase mb-5">Problem-3</h4>
+
+        <div className="d-flex justify-content-center gap-3">
+          <div>
+            <button
+              type="button"
+              className="btn text-white"
+              style={{ backgroundColor: "#46139f" }}
+              onClick={() => handleShow("all")}
+            >
+              All Contacts
+            </button>
+
+            {showModal && show === "all" && (
+              <Modal
+                handleClose={handleClose}
+                contacts={contacts}
+                usa={usa}
+                show={show}
+                setShow={setShow}
+              />
+            )}
+          </div>
+
+          <div>
+            <button
+              type="button"
+              className="btn text-white"
+              style={{ backgroundColor: "#ff7f50" }}
+              onClick={() => handleShow("us")}
+            >
+              US Contacts
+            </button>
+
+            {showModal && show === "us" && (
+              <Modal
+                handleClose={handleClose}
+                contacts={contacts}
+                usa={usa}
+                show={show}
+                setShow={setShow}
+              />
+            )}
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Problem2;
---
+++ src/components/problem-2/Modal.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Modal = ({handleClose, contacts, usa, show, setShow}) => {
+    return (
+        <div>
+            <h2>will show modal data</h2>
+        </div>
+    );
+};
+
+export default Modal;
